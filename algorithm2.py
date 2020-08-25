b75b792ad7c89f405a0c5667714ddfcaf9976c76
Nguyễn Bá Tùng <tungrs94@gmail.com>
2020-08-25 23:48:57 +0700

    update algorithm 2

--- algorithm2.py
+++ algorithm2.py
@@ -1,13 +1,18 @@
-x = 10
-y = 20
-count = 0
-while True:
-    if x < y:
-        x = x * 2
-    elif x > y:
-        x = x - 1
+x = int(input('enter value x:'))
+y = int(input('enter value y:'))
 
+
+def balance(x, y, count=0):
+    if y <= x:
+        return count + x - y
+
+    if y % 2 != 0:
+        y += 1
+        count += 1
+
+    y //= 2
     count += 1
-    if x == y:
-        break
-print(count)
+    return balance(x=x, y=y, count=count)
+
+
+print(balance(x, y))
